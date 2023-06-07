f7e44edbbf18639bc234c6f6ea42ba9e054e17a5
DougD-BY <doug.dekker@b-y.com>
2023-06-07 11:10:27 -0400

    Arrange to "Layered Architecture" folder structure

---
+++ P21.BusinessRule/BLL/Interfaces/IRuleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace P21.BusinessRule.BLL
+{
+	internal interface IRuleLogger
+	{
+		Type DeclaringType { get; }
+		BaseRule RuleToLog { get; }
+		LogLevel Threshold { get; }
+
+		void LogCritical(string criticalMessage, Exception exception);
+
+		IRuleLogger Setup(Configuration appConfiguration);
+	}
+}
--- P21.BusinessRule/BaseRule.cs
+++ P21.BusinessRule/BaseRule.cs
@@ -1,11 +1,26 @@
-﻿using P21.Extensions.BusinessRule;
+﻿using P21.BusinessRule.BLL;
+using P21.Extensions.BusinessRule;
 using System;
 
-namespace P21.BusinessRule.Model
+namespace P21.BusinessRule
 {
 	public abstract class BaseRule : Rule, IDisposable
 	{
+		private LogicLayer _logic;
 		private bool disposedValue;
+		internal IRuleLogger Logger { get; private set; }
+
+		internal LogicLayer Logic
+		{
+			get
+			{
+				if (_logic == null)
+				{
+					_logic = new LogicLayer(this, Logger);
+				}
+				return _logic;
+			}
+		}
 
 		public void Dispose()
 		{
@@ -18,7 +33,6 @@ namespace P21.BusinessRule.Model
 		{
 			return this.GetType().Name;
 		}
-
 		protected virtual void Dispose(bool disposing)
 		{
 			if (!disposedValue)
