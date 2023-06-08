f262a01fd0e2176cbd859de40ac261a56c041232
DougD-BY <doug.dekker@b-y.com>
2023-06-08 14:49:16 -0400

    Store application settings in a configuration file.

--- P21.BusinessRule/BLL/Interfaces/IRuleLogger.cs
+++ P21.BusinessRule/BLL/Interfaces/IRuleLogger.cs
@@ -6,10 +6,17 @@ namespace P21.BusinessRule.BLL
 	internal interface IRuleLogger
 	{
 		Type DeclaringType { get; }
+		bool Initialized { get; set; }
 		BaseRule RuleToLog { get; }
 		LogLevel Threshold { get; }
 
 		void LogCritical(string criticalMessage, Exception exception);
+
+		void LogDebug(string debugMessage);
+
+		void LogError(string errorMessage);
+
+		void LogWarning(string warningMessage);
 
 		IRuleLogger Setup(Configuration appConfiguration);
 	}
