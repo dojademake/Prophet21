2a1ba67bf28bd5591f5f18bd71138860b2130d80
DougD-BY <doug.dekker@b-y.com>
2023-06-08 14:49:16 -0400

    Store application settings in a configuration file.

--- P21.BusinessRule/BLL/LogicLayer.cs
+++ P21.BusinessRule/BLL/LogicLayer.cs
@@ -1,10 +1,14 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
 
 namespace P21.BusinessRule.BLL
 {
 	internal class LogicLayer
 	{
 		private Configuration _appConfig;
+		private string _p21Environment;
 
 		internal LogicLayer(BaseRule rule, IRuleLogger logger)
 		{
@@ -12,57 +16,59 @@ namespace P21.BusinessRule.BLL
 			Logger = logger;
 		}
 
-		private BaseRule Rule { get; }
+		public string BusinessRulesPath { get; private set; }
 		public IRuleLogger Logger { get; private set; }
 
-		//internal Configuration AppConfiguration
-		//{
-		//	get
-		//	{
-		//		if (_appConfig == null)
-		//		{
-		//			string fileName = Assembly.GetExecutingAssembly().Location;
-		//			string folderName = String.Empty;
-		//			string configPath = $"{fileName}.config";
-		//			if (File.Exists(configPath))
-		//			{
-		//				_appConfig = ConfigurationManager.OpenExeConfiguration(fileName);
-		//			}
-		//			else
-		//			{
-		//				try
-		//				{
-		//					folderName = BusinessRulesPath;
-		//					configPath = Path.Combine(folderName, Path.GetFileName(fileName));
-		//					if (File.Exists(configPath))
-		//						_appConfig = ConfigurationManager.OpenExeConfiguration(configPath);
-		//				}
-		//				catch (Exception ex)
-		//				{
-		//					if (LogInitialized)
-		//					{
-		//						Rule.HandleException(ex, MethodBase.GetCurrentMethod(), Rule);
-		//					}
-		//				}
-		//			}
-		//			if (string.IsNullOrWhiteSpace(configPath))
-		//			{
-		//				configPath = $"{folderName} - {fileName}";
-		//			}
-		//			if (_appConfig == null)
-		//			{
-		//				if (LogInitialized)
-		//				{
-		//					Rule.Logging.Log4Error($"Could not load the Business Rules Config file {configPath} for the {_p21Environment} environment");
-		//				}
-		//				else
-		//				{
-		//					throw new FileNotFoundException($"Could not load the Business Rules Config file for the {_p21Environment} environment", configPath);
-		//				}
-		//			}
-		//		}
-		//		return _appConfig;
-		//	}
-		//}
+		internal Configuration AppConfiguration
+		{
+			get
+			{
+				if (_appConfig == null)
+				{
+					string fileName = Assembly.GetExecutingAssembly().Location;
+					string folderName = String.Empty;
+					string configPath = $"{fileName}.config";
+					if (File.Exists(configPath))
+					{
+						_appConfig = ConfigurationManager.OpenExeConfiguration(fileName);
+					}
+					else
+					{
+						try
+						{
+							folderName = BusinessRulesPath;
+							configPath = Path.Combine(folderName, Path.GetFileName(fileName));
+							if (File.Exists(configPath))
+								_appConfig = ConfigurationManager.OpenExeConfiguration(configPath);
+						}
+						catch (Exception ex)
+						{
+							if (Logger.Initialized)
+							{
+								Rule.HandleException(ex, MethodBase.GetCurrentMethod(), Rule);
+							}
+						}
+					}
+					if (string.IsNullOrWhiteSpace(configPath))
+					{
+						configPath = $"{folderName} - {fileName}";
+					}
+					if (_appConfig == null)
+					{
+						if (Logger.Initialized)
+						{
+							Logger.LogError($"Could not load the Business Rules Config file {configPath} for the {_p21Environment} environment");
+						}
+						else
+						{
+							throw new FileNotFoundException($"Could not load the Business Rules Config file for the {_p21Environment} environment", configPath);
+						}
+					}
+				}
+				return _appConfig;
+			}
+		}
+
+		private BaseRule Rule { get; }
 	}
 }
