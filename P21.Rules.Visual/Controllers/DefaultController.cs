30f5c8b91cb7006b6ee682b34c4597d214ee9b98
DougD-BY <doug.dekker@b-y.com>
2023-08-21 17:59:40 -0400

    Create Default controller inheriting from P1.Extensions.Web.BaseRuleController

---
+++ P21.Rules.Visual/Controllers/DefaultController.cs
@@ -0,0 +1,90 @@
+﻿using P21.Extensions.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P21.Rules.Visual.Controllers
+{
+    public class DefaultController : BaseRuleController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Default/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Rule);
+        }
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View(Rule);
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
