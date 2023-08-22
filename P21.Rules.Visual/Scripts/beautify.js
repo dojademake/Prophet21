7654db08ff126f837c616d7273a8c3a93c905e7b
DougD-BY <doug.dekker@b-y.com>
2023-08-22 09:30:48 -0400

    Add ability to format XML display

---
+++ P21.Rules.Visual/Scripts/beautify.js
@@ -0,0 +1,21 @@
+﻿function beautifyXml(xml) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xml, 'text/xml');
+    const xsltDoc = parser.parseFromString([
+        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+        '  <xsl:strip-space elements="*"/>',
+        '  <xsl:template match="para[content-style][not(text())]">',
+        '    <xsl:value-of select="normalize-space(.)"/>',
+        '  </xsl:template>',
+        '  <xsl:template match="node()|@*">',
+        '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+        '  </xsl:template>',
+        '  <xsl:output method="xml" indent="yes"/>',
+        '</xsl:stylesheet>'
+    ].join('\n'), 'application/xml');
+    const xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsltDoc);
+    const resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+    const serializer = new XMLSerializer();
+    return serializer.serializeToString(resultDoc);
+}
