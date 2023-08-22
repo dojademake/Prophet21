a321deddbba24f9cda8d0a4fd0ebcaba8428516d
DougD-BY <doug.dekker@b-y.com>
2023-08-22 10:46:17 -0400

    Move javascript function to page level since SFTP site locks up the Scripts folder (not even file name versioning is allowed)

--- P21.Rules.Visual/App_Start/BundleConfig.cs
+++ P21.Rules.Visual/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace P21.Rules.Visual
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/beautify").Include(
-                        "~/Scripts/beautify.js"));
+                        "~/Scripts/beautify-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
