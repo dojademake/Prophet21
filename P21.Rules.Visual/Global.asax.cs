0328244f8f9b571f69fef40ae1dd9c92d19b7388
DougD-BY <doug.dekker@b-y.com>
2023-08-23 08:00:57 -0400

    Add rules list view

--- P21.Rules.Visual/Global.asax.cs
+++ P21.Rules.Visual/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using P21.Entity.Database;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,8 @@ namespace P21.Rules.Visual
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer<P21DbContext>(null);
         }
     }
 }
