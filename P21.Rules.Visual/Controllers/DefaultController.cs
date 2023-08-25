d4ab25d5fd0a22288c4e6ed44f1d3e5c71f751db
DougD-BY <doug.dekker@b-y.com>
2023-08-25 15:22:36 -0400

    Start copying test harness functionality to Create action

--- P21.Rules.Visual/Controllers/DefaultController.cs
+++ P21.Rules.Visual/Controllers/DefaultController.cs
@@ -2,8 +2,12 @@
 using P21.Entity.Services;
 using P21.Extensions.BusinessRule;
 using P21.Extensions.Web;
+using P21.Rules.Visual.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -43,11 +47,26 @@ namespace P21.Rules.Visual.Controllers
         // GET: Default/Create
         public ActionResult Create()
         {
+            if (!Rule.IsInitialized())
+            {
+                string content = FileUtility.ReadFileFromAppData("DefaultBusinessRule.xml");
+
+                if (content != null)
+                {
+                    SqlConnectionStringBuilder remoteConnection = new SqlConnectionStringBuilder(ConfigurationManager.AppSettings["RemoteConnectionString"]);
+                    Rule.Initialize(content, new Extensions.DataAccess.DBCredentials(remoteConnection.UserID, remoteConnection.Password, remoteConnection.DataSource, remoteConnection.InitialCatalog));
+                }
+                else
+                {
+                    return View("Error", new HandleErrorInfo(new Exception("Error initializing Web Visual Rule"), "Default", "Create"));
+                }
+            }
             return View(Rule);
         }
 
         // POST: Default/Create
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Create(FormCollection collection)
         {
             try
