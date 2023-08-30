8b0ed1247c4c04947359ba4044ed3f23c3fc7c5d
DougD-BY <doug.dekker@b-y.com>
2023-08-30 17:02:20 -0400

    Add WebApiConfig from baseline project

---
+++ P21.Rules.Visual/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace P21.Rules.Visual
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "TestApi",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { action = "Get" }
+            );
+
+            // WebAPI when dealing with JSON & JavaScript!
+            // Setup json serialization to serialize classes to camel (std. Json format)
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ lib/P21.Extensions.20.1.4018/readme.md
@@ -0,0 +1,3 @@
+# Requirements
+
+`P21.Extensions.dll` v20.1.4018 should be in this folder.  Download it from the middleware site.
