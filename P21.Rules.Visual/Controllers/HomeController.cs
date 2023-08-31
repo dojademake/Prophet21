531e7a635134cd7905bcceb06fc30dd12059a4a7
DougD-BY <doug.dekker@b-y.com>
2023-08-31 14:32:20 -0400

    Copy baseline over fresh build: Initialize works once.

---
+++ P21.Rules.Visual/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P21.Rules.Visual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Prophet 21 Visual Business Rules Template";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetStarted()
+        {
+            return View();
+        }
+
+    }
+}
--- P21.Rules.Visual/Global.asax.cs
+++ P21.Rules.Visual/Global.asax.cs
@@ -2,12 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.SessionState;
 
 namespace P21.Rules.Visual
 {
@@ -15,7 +15,6 @@ namespace P21.Rules.Visual
     {
         protected void Application_Start()
         {
-
             GlobalFilters.Filters.Add(new HandleErrorAttribute());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -23,8 +22,8 @@ namespace P21.Rules.Visual
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // TOOD: implement to determine why initialize is throwing 404 from https://stackoverflow.com/questions/48778587/mvc-routeconfig-catchall-url-not-working
-            //ControllerBuilder.Current.SetControllerFactory(new NotFoundControllerFactory());
+            // from https://stackoverflow.com/questions/48778587/mvc-routeconfig-catchall-url-not-working
+            ControllerBuilder.Current.SetControllerFactory(new NotFoundControllerFactory());
 
             Database.SetInitializer<P21DbContext>(null);
         }
