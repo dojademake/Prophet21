242bd60af0ec889f3a28f8dd829c4df44370c3dc
DougD-BY <doug.dekker@b-y.com>
2023-09-01 15:48:54 -0400

    ﻿Create SampleRule1 Area and Controller

---
+++ P21.Rules.Visual/Areas/SampleRule1/Controllers/SampleRule1Controller.cs
@@ -0,0 +1,29 @@
+﻿
+using P21.Extensions.Web;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace P21.Rules.Visual.Areas.SampleRule1.Controllers
+{
+    public class SampleRule1Controller : BaseRuleController
+    {
+        // GET: SampleRule1/SampleRule1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public ActionResult Return()
+		{
+			Rule.RuleResult.Success = true;
+
+			//IMPORTANT - This is what returns the Visual Rule control back to the server
+			//DO NOT REMOVE
+			return RedirectToAction("Close", "Initialize", new { area = "" });
+		}
+    }
+
+}
