35a3c249bc87e3748312216bc1acde3a9a7deaf8
DougD-BY <doug.dekker@b-y.com>
2023-09-01 15:48:54 -0400

    Create SampleRule1 Area and Controller

---
+++ P21.Rules.Visual/Areas/SampleRule1/SampleRule1AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace P21.Rules.Visual.Areas.SampleRule1
+{
+    public class SampleRule1AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SampleRule1";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SampleRule1_default",
+                "SampleRule1/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
